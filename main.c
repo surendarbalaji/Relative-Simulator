cbab4fc48c1d75e571b5b89caf497395581dcbba
Surendar Balaji <sbalaji07@icloud.com>
2026-01-22 17:38:22 +0000

    simple raylib grid

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include "raylib.h"
+#include "rlgl.h"
 
 int main(void) {
 
     const int screenWidth = 800;
     const int screenHeight = 450;
 
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(screenWidth, screenHeight, "Relative Simulator");
+
+    Camera2D camera = { 0 };
+    camera.zoom = 1.0f;
 
     SetTargetFPS(60);
 
@@ -13,9 +18,16 @@ int main(void) {
 
         BeginDrawing();
 
-        ClearBackground(BLACK);
+            ClearBackground(WHITE);
 
-        DrawText("Relative Simulator", 190, 200, 20, RAYWHITE);
+        BeginMode2D(camera);
+            rlPushMatrix();
+                rlRotatef(-90, 1, 0, 0);
+                DrawGrid(100, 50);
+            rlPopMatrix();
+        EndMode2D();
+
+            DrawText("Relative Simulator", 190, 210, 20, RAYWHITE);
 
         EndDrawing();
     }
